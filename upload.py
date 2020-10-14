fcaa8d97f76bbf2bcc7a3ceb3470949ab25d698b
justin0u0 <mail@justin0u0.com>
2020-10-15 02:31:16 +0800

    Initial Commit

---
+++ upload.py
@@ -0,0 +1,53 @@
+import requests
+import getpass
+
+url = 'https://acm.cs.nthu.edu.tw/users/login/'
+username = input('Username: ')
+password = getpass.getpass('Password: ')
+
+session = requests.session()
+r = session.get(url)
+
+payload = {
+  'username': username,
+  'password': password,
+  'csrfmiddlewaretoken': session.cookies['csrftoken']
+}
+
+headers = {
+  'Referer': url,
+}
+
+res = session.post(url, headers=headers, data=payload)
+
+if res.status_code == 200:
+  print('Login successfully')
+
+problem_id = input('Problem ID: ')
+upload_url = f'https://acm.cs.nthu.edu.tw/problem/{problem_id}/testcase/'
+
+headers = {
+  'Referer': f'https://acm.cs.nthu.edu.tw/problem/{problem_id}/edit/',
+  'X-CSRFToken': session.cookies['csrftoken']
+}
+
+testcases = int(input('Number of testcases: '))
+
+for testcase in range(testcases):
+  file_name = f'./data/{problem_id}_{str(testcase).zfill(2)}'
+  print(f'Uploading {file_name}.in, {file_name}.out ...')
+
+  files = {
+    't_in': open(f'{file_name}.in', 'rb'),
+    't_out': open(f'{file_name}.out', 'rb')
+  }
+
+  payload = {
+    'time_limit': 1,
+    'memory_limit': 1
+  }
+
+  res = session.post(upload_url, headers=headers, files=files, data=payload)
+
+  if res.status_code == 200:
+    print(f'Upload {file_name}.in, {file_name}.out successfully')
